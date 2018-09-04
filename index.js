3e65b4b5d9e231998cc2804a602ee7b4d6962647
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-04 07:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-// Code your solution in this file.
-const knownDecepticons = ['Megatron', 'Skywarp', 'Laserbeak', 'Barricade'];
-
-const robots = [
-  { name: 'Bumblebee', alliance: null },
-  { name: 'Laserbeak', alliance: null },
-  { name: 'Barricade', alliance: null },
-  { name: 'Optimus Prime', alliance: null },
-  { name: 'Skywarp', alliance: null },
-  { name: 'Megatron', alliance: null },
-  { name: 'Ironhide', alliance: null },
-  { name: 'Ratchet', alliance: null }
-];
-
-const sortedRobots = robots.map(robot => {
-  const isDecepticon = knownDecepticons.includes(robot.name);
-  return Object.assign({}, robot, {
-    alliance: isDecepticon ? 'decepticon' : 'autobot'
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
   });
-});
+}
 
-const zebraStripes = [
-  { width: 9.12, color: null },
-  { width: 5.71, color: null },
-  { width: 6.01, color: null },
-  { width: 1.54, color: null },
-  { width: 8.34, color: null },
-  { width: 7.77, color: null },
-  { width: 0.59, color: null },
-  { width: 7.31, color: null },
-];
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
 
-const coloredZebraStripes = zebraStripes.map((stripe, index) => {
-  const isEven = (index % 2) === 0;
-  return Object.assign({}, stripe, {
-    color: isEven ? 'black' : 'white'
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
   });
-});
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
